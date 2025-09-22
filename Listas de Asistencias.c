63c22b4efed89b2dccda3989179ee1b84b07dac0
Serch936 <ssergiogs@hotmail.com>
2025-09-22 13:39:20 -0600

    Implementación de las estructuras

--- Listas de Asistencias.c
+++ Listas de Asistencias.c
@@ -1,5 +1,28 @@
 #include<stdio.h>
+#include<stdbool.h>
+
+
+typedef struct alumno{
+    char *nombre;
+    bool asistencia;
+    TAlumno *siguiente;
+    TAlumno *anterior;
+}TAlumno;
+
+typedef struct lista{
+    TDia *siguiente;
+    TDia *anterior;
+    TAlumno *alumnos;
+    int cantidad;
+}TLista;
+
+typedef struct dia{
+    TDia *arriba;
+    TDia *abajo;
+    TLista *alumnosDelDía;
+    int día
+}TDia;
 
 int main(){
-    
+    return 0;
 }
