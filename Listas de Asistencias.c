b2f3c98858fef1ad379fc4ab1c4c136f6ad7bd5c
Serch936 <ssergiogs@hotmail.com>
2025-09-22 14:52:22 -0600

    Creación de Menú

--- Listas de Asistencias.c
+++ Listas de Asistencias.c
@@ -12,7 +12,7 @@ typedef struct alumno{
 typedef struct lista{
     TDia *siguiente;
     TDia *anterior;
-    TAlumno *alumnos;
+    TAlumno *alumnos; //Arreglo dinamico de alumnos
     int cantidad;
 }TLista;
 
@@ -48,10 +48,51 @@ int main(){
     TAlumno a;
     TLista l;
     TDia d;
+    int opcion;
 
     inicializa_alumno(&a);
     inicializa_lista(&l);
     inicializa_dia(&d);
+
+    do{
+        printf("---MENU---\n");
+        printf("1. Agregar alumno\n");
+        printf("2. Eliminar alumno\n");
+        printf("3. Marcar asistencias\n");
+        printf("4. Mostrar lista de asistencias de un dia\n");
+        printf("5. Mostrar lista de asistencias de un alumno\n");
+        printf("6. Salir\n");
+        printf("Ingrese una opcion: ");
+        scanf("%d", &opcion);
+
+        switch(opcion){
+            case 1:
+                // Agregar alumno
+                break;
+            case 2:{
+                if(l.cantidad == 0){
+                    printf("No hay alumnos en la lista.\n");
+                    break;
+                }
+
+                break;
+            }
+            case 3:
+                // Marcar asistencias
+                break;
+            case 4:
+                // Mostrar lista de asistencias de un dia
+                break;
+            case 5:
+                // Mostrar lista de asistencias de un alumno
+                break;
+            case 6:
+                printf("Saliendo del programa...\n");
+                break;
+            default:
+                printf("\nOpcion invalida \nIntentelo nuevamente\n");
+        }
+    }while(opcion != 6);
     
     return 0;
 }
