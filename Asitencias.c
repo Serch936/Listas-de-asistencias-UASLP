35782366143dc907f952b5d3ae9d25c410c2ed0a
Sergio Andrés Guerrero Serrato <158009559+Serch936@users.noreply.github.com>
2025-09-30 13:54:19 -0600

    Creación de funciones de ordenamiento
    Se creó las funciones para ordenar tanto alfabéticamente como por el ID del alumno.

--- Asitencias.c
+++ Asitencias.c
@@ -29,6 +29,8 @@ typedef struct dia{
     TALUMNO *lista_alumnos;
 }TDIA;
 
+void ordenar_alfabeticamente(TDIA *cab);
+void ordenar_por_ID(TDIA *cab);
 void mostrar_listas(TDIA *cab);
 void crea_dia(TDIA **cab_dia, TALUMNO *lista_original, TFECHA fecha);
 void agrega_alumnos(TALUMNO *cab, TALUMNO *inicio, int cantidad_alumnos);
@@ -60,7 +62,8 @@ int main(){
         printf("\n---MENU---\n");
         printf("1. Mostrar listas\n");
         printf("2. Tomar asistencia\n");
-        printf("3. Salir\n");
+        printf("3. Ordenar alumnos");
+        printf("4. Salir\n");
         printf("Elige una opcion: ");
         scanf("%i", &opcion);
         getchar();
@@ -81,20 +84,23 @@ int main(){
                     scanf("%i", &opc);
 
                     switch(opc){
-                        case 1:
+                        case 1:{
                             mostrar_listas(&cab);
                             break;
-                        case 2:
+                        }
+                        case 2:{
                             printf("Ingresa el nombre del alumno a buscar: ");
                             gets(nombre);
                             //buscar_alumno(cab, nombre);
                             break;
-                        case 3:
+                        }
+                        case 3:{
                             printf("Ingresa el numero del dia a buscar: ");
                             int dia;
                             scanf("%i", &dia);
                             //buscar_dia(cab_dia, dia);
                             break;
+                        }
                         default:
                             printf("Opcion no valida\n");
                     }
@@ -115,10 +121,24 @@ int main(){
                 }
                 break;
             }
-            case 3:
-                dia++;
-                tomarAsistencia(cabeza, dia);
+            case 3:{
+                int opc;
+                
+                printf("---MENU ORDENAR---\n");
+                printf("1. Ordenar alfabeticamente\n");
+                printf("2. Ordenar por ID\n");
+                printf("Elige una opcion: ");
+                scanf("%i", &opc);
+
+                switch(opc){
+                    case 1:{
+                        ordenar_alfabeticamente(&cab);
+                        break;
+                    }
+                }
+
                 break;
+            }
             case 4:
                 printf("Saliendo...\n");
                 break;
@@ -138,6 +158,122 @@ int main(){
 }
 
 /*--- Definiciones de Funciones ---*/
+
+void ordenar_alfabeticamente(TDIA *cab){
+    TDIA *dia_aux;
+    TALUMNO *alumno_i, *alumno_j;
+    TNOMBRE aux_nombre;
+    int aux_ID;
+    bool aux_asistencia;
+
+    dia_aux = cab;
+
+    if(dia_aux == NULL){
+        printf("La lista de dias esta vacia\n");
+        return;
+    }
+
+    while(dia_aux != NULL){
+        alumno_i = dia_aux->lista_alumnos;
+
+        if(alumno_i == NULL){
+            printf("La lista de alumnos esta vacia\n");
+            return;
+        }
+
+        while(alumno_i != NULL){
+            alumno_j = alumno_i->siguiente;
+
+            while(alumno_j != NULL){ //Aquí el pedo es si lo quieren ordenar por apellidos o nombres, sino solo se debe cambiar el if con los apellidos :p
+                if(strcmp(alumno_i->nombre.nombres, alumno_j->nombre.nombres) > 0){
+                    strcpy(aux_nombre.nombres, alumno_i->nombre.nombres);
+                    strcpy(aux_nombre.apell_p, alumno_i->nombre.apell_p);
+                    strcpy(aux_nombre.apell_m, alumno_i->nombre.apell_m);
+
+                    strcpy(alumno_i->nombre.nombres, alumno_j->nombre.nombres);
+                    strcpy(alumno_i->nombre.apell_p, alumno_j->nombre.apell_p);
+                    strcpy(alumno_i->nombre.apell_m, alumno_j->nombre.apell_m);
+
+                    strcpy(alumno_j->nombre.nombres, aux_nombre.nombres);
+                    strcpy(alumno_j->nombre.apell_p, aux_nombre.apell_p);
+                    strcpy(alumno_j->nombre.apell_m, aux_nombre.apell_m);
+
+                    aux_ID = alumno_i->ID;
+                    alumno_i->ID = alumno_j->ID;
+                    alumno_j->ID = aux_ID;
+
+                    aux_asistencia = alumno_i->asistencias;
+                    alumno_i->asistencias = alumno_j->asistencias;
+                    alumno_j->asistencias = aux_asistencia;
+                }
+
+                alumno_j = alumno_j->siguiente;
+            }
+
+            alumno_i = alumno_i->siguiente;
+        }
+
+        dia_aux = dia_aux->siguiente;
+    }
+}
+
+void ordenar_por_ID(TDIA *cab){
+    TDIA *dia_aux;
+    TALUMNO *alumno_i, *alumno_j;
+    TNOMBRE aux_nombre;
+    int aux_ID;
+    bool aux_asistencia;
+
+    dia_aux = cab;
+
+    if(dia_aux == NULL){
+        printf("La lista de dias esta vacia\n");
+        return;
+    }
+
+    while(dia_aux != NULL){
+        alumno_i = dia_aux->lista_alumnos;
+
+        if(alumno_i == NULL){
+            printf("La lista de alumnos esta vacia\n");
+            return;
+        }
+
+        while(alumno_i != NULL){
+            alumno_j = alumno_i->siguiente;
+
+            while(alumno_j != NULL){
+                if(alumno_i->ID > alumno_j->ID){
+                    strcpy(aux_nombre.nombres, alumno_i->nombre.nombres);
+                    strcpy(aux_nombre.apell_p, alumno_i->nombre.apell_p);
+                    strcpy(aux_nombre.apell_m, alumno_i->nombre.apell_m);
+
+                    strcpy(alumno_i->nombre.nombres, alumno_j->nombre.nombres);
+                    strcpy(alumno_i->nombre.apell_p, alumno_j->nombre.apell_p);
+                    strcpy(alumno_i->nombre.apell_m, alumno_j->nombre.apell_m);
+
+                    strcpy(alumno_j->nombre.nombres, aux_nombre.nombres);
+                    strcpy(alumno_j->nombre.apell_p, aux_nombre.apell_p);
+                    strcpy(alumno_j->nombre.apell_m, aux_nombre.apell_m);
+
+                    aux_ID = alumno_i->ID;
+                    alumno_i->ID = alumno_j->ID;
+                    alumno_j->ID = aux_ID;
+
+                    aux_asistencia = alumno_i->asistencias;
+                    alumno_i->asistencias = alumno_j->asistencias;
+                    alumno_j->asistencias = aux_asistencia;
+                }
+
+                alumno_j = alumno_j->siguiente;
+            }
+
+            alumno_i = alumno_i->siguiente;
+        }
+
+        dia_aux = dia_aux->siguiente;
+    }
+}
 
 void mostrar_listas(TDIA *cab){
     TDIA *dia_aux;
@@ -160,6 +296,9 @@ void mostrar_listas(TDIA *cab){
             printf("\tID: %i, Nombre: %s %s %s, Asistencia: %s\n", alumno_aux->ID, alumno_aux->nombre.nombres, alumno_aux->nombre.apell_p, alumno_aux->nombre.apell_m, alumno_aux->asistencias? "Presente" : "Ausente"); //Nms, no sabía que se podía poner un "if" en una printf de esa manera :o
             alumno_aux = alumno_aux->siguiente;
         }
+
+        dia_aux = dia_aux->siguiente;
+        printf("\n");
     }
 }
 
